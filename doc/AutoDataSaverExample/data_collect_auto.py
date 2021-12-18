973c4fa1288466c04f45057fb50d2e2126cff9ae
eneserciyes <merciyes18@ku.edu.tr>
2021-12-18 15:28:54 +0300

    carla server utils added

--- doc/AutoDataSaverExample/data_collect_auto.py
+++ doc/AutoDataSaverExample/data_collect_auto.py
@@ -1,7 +1,11 @@
 import subprocess
 import os
+import carlatools.server_utils.server_utils as server
 
 if __name__ == "__main__":
+    server_manager = server.CarlaServerManager("/home/fguney/CarlaUE4.sh")
+    server_manager.start()
+
     for route in list(os.scandir("/home/fguney/projects/e2e-drive/leaderboard/data/routes_devtest")):
         print(os.path.abspath(route))
         os.environ["ROUTES"] = os.path.abspath(route)
